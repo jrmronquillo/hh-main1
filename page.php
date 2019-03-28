93deb019e33030acbf2db379c7b08bf546ca89a7
System Administrator <root@ELSML03JR909A-5.local>
2019-03-28 08:28:35 -0700

    removed excess html from page.php

--- page.php
+++ page.php
@@ -55,10 +55,9 @@
 	<?php
 	while(have_posts()) {
 		the_post(); ?>
-		<h1> Page Title</h1>
 			<h2> <?php the_title() ?></h2>
 			<p><?php the_content() ?></p>
-		<?php }
-
+		
+	<?php }
 	get_footer();
-?>
+	?>
