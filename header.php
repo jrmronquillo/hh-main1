8873a837fe25b5f89d49ff804e5e9297f89ebe7e
System Administrator <root@ELSML03JR909A-5.local>
2019-03-28 09:04:29 -0700

    added meta tag for viewport to fix navbar scaling issue

--- header.php
+++ header.php
@@ -3,6 +3,6 @@
 <html>
 	<head>
 		<?php wp_head(); ?>
-		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 	</head>
 	<body class='text-center'>
--- page.php
+++ page.php
@@ -14,10 +14,8 @@
 	<?php
 		//wp_list_pages();
 	 ?>
-
-	 <div class="cover-container d-flex w-100 h-100 mx-auto flex-column">
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	  <a class="navbar-brand" href="#">Home</a>
+	 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+	  <a class="navbar-brand" href="<?php echo site_url('/') ?>">Home</a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
@@ -25,7 +23,7 @@
 	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
 	    <ul class="navbar-nav mr-auto">
 	      <li class="nav-item active">
-	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	        <a class="nav-link" href="#">Portfolio <span class="sr-only">(current)</span></a>
 	      </li>
 	      <li class="nav-item">
 	        <a class="nav-link" href="#">Link</a>
@@ -51,6 +49,9 @@
 	    </form>
 	  </div>
 	</nav>
+
+	 <div class="cover-container d-flex w-100 h-100 mx-auto flex-column">
+	
 	
 	<?php
 	while(have_posts()) {
