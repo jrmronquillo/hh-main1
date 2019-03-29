d6b174d4992805fabe2aeae22a8c54a0a03482c3
System Administrator <root@ELSML03JR909A-5.local>
2019-03-29 08:15:30 -0700

    home template added

---
+++ page-2.php
@@ -0,0 +1,27 @@
+<?php /* Template name: home */ ?>
+
+<?php get_header(); ?>
+<div class="cover-container d-flex w-100 h-100 mx-auto flex-column">
+	
+ 
+
+	<main role="main" class="inner cover blurry-city-bg">
+		<header class="header-style"></header>
+				<span class="main-text indie-google-font"> Lorem Ipsum! </span>
+				<div class="">
+					<button type="button" class=" btn btn-outline-warning button-custom ">
+						<a href='<?php echo site_url('/test-p/') ?>'> <div class="button-text indie-google-font"> Test </div> </a>
+					</button>
+				</div>
+			<button type="button" class="btn btn-outline-warning cta-margin">
+				<a href="http://google.com/"> <div class="button-text indie-google-font">Action 2 </div></a>
+			</button>
+	</main>
+	
+</div>
+</
+
+<!--<?php	
+	//get_footer();
+
+?> -->
