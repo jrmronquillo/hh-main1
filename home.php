f4c85ed8ef3aa9fe56cc35b2afa3dd79c1de16bc
jrmronquillo <jrm.ronquillo@gmail.com>
2019-03-29 14:06:26 -0700

    changed the main page bg image

--- home.php
+++ home.php
@@ -2,26 +2,26 @@
 
 <?php get_header(); ?>
 <div class="cover-container d-flex w-100 h-100 mx-auto flex-column">
-	
- 
 
-	<main role="main" class="inner cover blurry-city-bg">
+
+
+	<main role="main" class="inner cover sunrise-bg">
 		<header class="header-style"></header>
-				<span class="main-text indie-google-font"> Lorem Ipsum! </span>
+				<span class="main-text indie-google-font"> <?php the_title() ?> </span>
 				<div class="">
-					<button type="button" class=" btn btn-outline-warning button-custom ">
-						<a href='<?php echo site_url('/test-p/') ?>'> <div class="button-text indie-google-font"> Test </div> </a>
+					<button type="button" class=" btn btn-outline-secondary button-custom ">
+						<a href='<?php echo site_url('/portfolio/') ?>'> <div class="button-text indie-google-font"> Portfolio </div> </a>
 					</button>
 				</div>
-			<button type="button" class="btn btn-outline-warning cta-margin">
-				<a href="http://google.com/"> <div class="button-text indie-google-font">Action 2 </div></a>
+			<button type="button" class="btn btn-outline-secondary cta-margin">
+				<a href="http://instagram.com/highlighthumble"> <div class="button-text indie-google-font"> Instagram </div></a>
 			</button>
 	</main>
-	
+
 </div>
 </
 
-<!--<?php	
+<!--<?php
 	//get_footer();
 
 ?> -->
